96f55c31cb3d2de4cef136300a1cb48d27259795
LyricalHero <zholob.r@gmail.com>
2019-02-20 15:41:24 +0200

    another one

---
+++ Hillel/Lesson1/DataTypes/DataTypes/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataTypes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello  ");
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Min: " +int.MinValue + "Max: " int.MaxValue);
+            //$ -интегрирует код в строку 
+            MessageBox.Show($"Min: {int.MinValue}, Max: {int.MaxValue}");
+
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {decimal.MinValue}, Max: {decimal.MaxValue}");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {double.MinValue}, Max: {double.MaxValue}");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {float.MinValue}, Max: {float.MaxValue}");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {ushort.MinValue}, Max: {ushort.MaxValue}");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {byte.MinValue}, Max: {byte.MaxValue}");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {long.MinValue}, Max: {long.MaxValue}");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Min: {short.MinValue}, Max: {short.MaxValue}");
+        }
+    }
+}
---
+++ Hillel/Lesson1/HomeWork_2/HomeWork_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+//using static System.Environment;
+/*С помощью класса System.Environment вывести на консоль сведенья о компьютере*/
+
+namespace HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string system;
+            
+            WriteLine("Имя и Аргументы коммандной строки когда Ваше приложение было запущенно: " + Environment.CommandLine);
+            
+            WriteLine("Путь к приложению: " + Environment.CurrentDirectory);
+           // system = Environment.MachineName;
+            WriteLine("\nИмя компьютера: " + Environment.MachineName);
+            WriteLine("Версия ОС: " + Environment.OSVersion);
+            WriteLine("Колличество процессоров: " + Environment.ProcessorCount);
+            WriteLine("Путь к системному каталогу: " + Environment.SystemDirectory);
+            WriteLine("Время с последнего запуска \a системы: " + (Environment.TickCount)/1000/60 + " минут");
+            WriteLine("Имя пользователя: {0}", Environment.UserName );
+
+           // WriteLine(String.Join( ',', Environment.GetLogicalDrives) );
+
+
+
+
+            ReadLine();
+        }
+    }
+}
