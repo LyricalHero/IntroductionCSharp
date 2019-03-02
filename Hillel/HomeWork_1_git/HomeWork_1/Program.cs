6e310d831e0e16baf04c01b75544a352410746db
lyricalhero <35657013+LyricalHero@users.noreply.github.com>
2019-03-02 15:42:05 +0200

    Исправил Task 0

--- Hillel/HomeWork_1_git/HomeWork_1/Program.cs
+++ Hillel/HomeWork_1_git/HomeWork_1/Program.cs
@@ -1,23 +1,24 @@
-﻿using System;
+using System;
 using static System.Console;
 
 /*Написать программу для отображения Ваших данных – ФИО, сколько лет и т.д. ​(Обязательно каждые данные отображать через переменные)*/
 
-namespace Task0
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-//Инициализация переменных, возраст решил через 'byte' для экономии памяти 
+namespace Task0 {
+    class Program {
+        static void Main(string[] args) {
+            //Инициализация переменных, возраст решил через 'byte' для экономии памяти 
             string FirstName = "Роман", LastName = "Жолоб", MiddleName = "Владимирович";
-            string DateOfBirth = "25 января 1994";
+            string MonthOfBirth = " января ";
+            byte DayOfBirth = 25, YearOfBirth = 1994;
+
             byte Age = 25;
-// вывод инфы
+            // вывод инфы
             WriteLine("Коротко о себе:\nФИО:\t" + LastName + " " + FirstName + " " + MiddleName);
-            WriteLine("Дата рождения:\t" + DateOfBirth + "\nМне " + Age + " лет.\nПриятно познакомится :)");
+            WriteLine("Дата рождения:\t" + NumberBirth + DateOfBirth + YearOfBirth + "\nМне " + Age + " лет.\nПриятно познакомится :)");
 
             ReadLine();
         }
     }
 }
+
+  
