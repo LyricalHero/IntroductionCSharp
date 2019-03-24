8f0480efa92a0bd907b6b24627cd0a3aed5e4ffd
LyricalHero <zholob.r@gmail.com>
2019-03-24 18:36:06 +0200

    Добавил ДЗ 6

---
+++ Hillel/HomeWork_6_Double_Array/HomeWork_6_Double_Array/HomeWork_6.cs
@@ -0,0 +1,57 @@
+﻿ using System;
+using static System.Console;
+
+
+
+
+namespace HomeWork_6_Double_Array {
+    class HomeWork_6 {
+        static void Main(string[] args) {
+            LogicProgramm();
+
+            ReadLine();
+        }
+        //метод в которм вля логика программы что бы не засорять Main
+        private static void LogicProgramm() {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
