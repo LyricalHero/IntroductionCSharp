bf2cd6d59c9c208818e8efe3be779b587acbbd8c
LyricalHero <zholob.r@gmail.com>
2019-04-13 09:13:09 +0300

    Add Lesson8

---
+++ Hillel/Lesson8/Lesson8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using static System.Console;
+
+namespace Lesson8 {
+    class Program {
+        static void Main(string[] args) {
+
+
+
+            ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
